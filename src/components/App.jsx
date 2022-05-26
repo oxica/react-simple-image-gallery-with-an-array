8373634aa891284869e42e2d1e2f4048deeb7db9
Oksana <kskaplun@gmail.com>
2022-05-26 18:53:42 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import ImgGallery from './ImgGallery/ImgGallery';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <ImgGallery />
     </div>
   );
 };
---
+++ src/components/ImgGallery/ImgGallery.jsx
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+import img from './img.json';
+
+class ImgGallery extends Component {
+  render() {
+    return (
+      <>
+        <center>
+          <h1>Image gallery</h1>
+          <div>
+            {img.map(({ id, imgSrc }) => (
+              <img
+                key={id}
+                src={imgSrc}
+                alt="images1"
+                height="250"
+                width="250"
+                style={{ border: 'solid' }}
+              />
+            ))}
+          </div>
+        </center>
+      </>
+    );
+  }
+}
+
+export default ImgGallery;
